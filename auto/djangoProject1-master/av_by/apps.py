8af6fc07ac793ace8638b238a409429f9b57fd63
17pavel <59541732+17pavel@users.noreply.github.com>
2024-04-05 22:53:08 +0300

    Revert "test"

--- auto/djangoProject1-master/av_by/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class AvByConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'av_by'
--- auto/untitled-master/src/Sity_region.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function Sity_region(props) {
-    return (
-        <div>
-            {props.city.name}
-        </div>
-    )
-}
